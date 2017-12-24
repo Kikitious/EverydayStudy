1fb2f16f8e9c3e5610256926bef80d303b65ab43
duhongliu <duhongliu@travelsky.com>
2017-12-24 15:25:13 +0800

    希尔排序

--- app/src/main/java/com/katherine/du/everydaystudy/up20171223/Sort.java
+++ app/src/main/java/com/katherine/du/everydaystudy/up20171223/Sort.java
@@ -11,7 +11,8 @@ public class Sort {
         int[] a = new int[]{60, 71, 49, 11, 82, 49, 3, 66};
 
 
-        printArray(directInsertSort(a));
+        //printArray(directInsertSort(a));
+        printArray(shellSort(a));
 
     }
 
@@ -21,6 +22,26 @@ public class Sort {
         }
     }
 
+
+    public static int[] shellSort(int[] array) {
+        int n = array.length;
+        int i, j, d;
+        int temp;
+        d = n / 2;
+        while (d >= 1) {
+            for (i = d; i < n; i++) {
+                temp = array[i];
+                j = i - d;
+                while (j >= 0 && temp < array[j]) {
+                    array[j + d] = array[j];
+                    j = j - d;
+                }
+                array[j + d] = temp;
+            }
+            d = d / 2;
+        }
+        return array;
+    }
 
     public static int[] directInsertSort(int[] array) {
         int n = array.length;
