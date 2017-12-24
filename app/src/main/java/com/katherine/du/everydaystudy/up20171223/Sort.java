d6e1d216704b7286f168f9ed16a6a832ef91432a
duhongliu <duhongliu@travelsky.com>
2017-12-24 19:31:03 +0800

    快速排序

--- app/src/main/java/com/katherine/du/everydaystudy/up20171223/Sort.java
+++ app/src/main/java/com/katherine/du/everydaystudy/up20171223/Sort.java
@@ -11,9 +11,10 @@ public class Sort {
         int[] a = new int[]{60, 71, 49, 11, 82, 49, 3, 66};
 
 
-        directInsertSort(a);
+//        directInsertSort(a);
         //shellSort(a);
 //        bubbleSort(a);
+        printArray(quickSort(a, 0, a.length - 1));
 
     }
 
@@ -22,6 +23,29 @@ public class Sort {
             System.out.print(array[i] + "  ");
         }
         System.out.println();
+    }
+
+    public static int[] quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int i = low;
+            int j = high;
+            int key = array[i];
+            while (i < j) {
+                while (i < j && key <= array[j]) {
+                    j--;
+                }
+                array[i] = array[j];
+                while (i < j && key >= array[i]) {
+                    i++;
+                }
+                array[j] = array[i];
+            }
+            array[i] = key;
+            printArray(array);
+            quickSort(array, low, i - 1);
+            quickSort(array, i + 1, high);
+        }
+        return array;
     }
 
     public static int[] bubbleSort(int[] array) {
